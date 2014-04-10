b260677562ac0f12679c736e768e4c9d71557846
nelsonmendes <nelson.daniel.mendes@gmail.com>
2014-04-10 18:24:42 +0100

    done dashboard and main layout#

---
+++ pages/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+	<meta name="description" content=""/>
+	<title>dashboard</title>
+	<!-- START Javascript -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+	<!-- START CSS -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300' rel='stylesheet' type='text/css'>
+	<link href='../css/style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="mobile-header">
+	<ul class="clearfloat">
+		<li>
+			<a href="">Home</a>
+		</li>
+		<li>
+			<a href="">Profile</a>
+		</li>
+		<li>
+			<a href="">Explore</a>
+		</li>
+		<li>
+			<a href="">Contacts</a>
+		</li>
+		
+	</ul>
+</div>
+
+<div id="container-outer">
+	
+	<div id="container">
+		<div id="container-inner">
+			<h1>Nelson Mendes</h1>
+			<h2>Coisas dele</h2>
+			<p>
+				Listar os percursos dele
+			</p>
+		</div>
+	</div>
+
+</div>
+</body>
+</html>
