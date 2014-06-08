17e3c5113512ed7c0c8078b4a84acf4a59557c94
Rui Gonçalves <rui.nf1@gmail.com>
2014-06-08 19:04:41 +0100

    Revert "Corrected login"
    This reverts commit d0e6901b445c1407d84ff7610fed7bf49549c0ba.

--- appengine/api/receivedata.py
+++ appengine/api/receivedata.py
@@ -2,23 +2,15 @@ import webapp2
 from datastore import *
 from google.appengine.ext import db
 from pages import BaseHandler
+from xml.dom import minidom
 
-class RecieveData(BaseHandler):
 
-	def post(self):
-		title = self.request.get("title")
-		#photo = self.request.get("photo")
-		ingredients = []
-		if len(ingString) > 0:
-			ingredients = ingString.split(",")
-		recipe = self.request.get("recipe")
-		email = self.session.get("user")
-		user = searchUserByEmail(email)
-		postId = addPost(user, title, photo)
-		if postId is not None:
-			"""addIngredients(postId, ingredients)
-			for ing in ingredients:
-				addIngredient(ing,None)"""
-			if len(recipe) > 0:
-				addRecipe(postId,recipe)
-		return self.redirect('/')
+class ReceiveData(BaseHandler):
+
+	def xmldata(self):
+		xmldoc = minidom.parse('track_gpx.xml')
+		itemlist = xmldoc.getElementsByTagName('trkpt') 
+		print len(itemlist)
+		print itemlist[0].attributes['lon'].value
+		for s in itemlist :
+		    print s.attributes['lon'].value
